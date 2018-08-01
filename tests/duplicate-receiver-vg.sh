3a40a97922e911828919a73047f58d6b937add42
PascalWithopf <pwithopf@adiscon.com>
2018-08-01 08:38:41 +0200

    add test for duplicate receiver error
    In rsyslog configuring two identical relp receiver leads
    to an error. The test checks that everything is working
    fine within librelp.
    
    See: https://github.com/rsyslog/rsyslog/issues/2874

---
+++ tests/duplicate-receiver-vg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ `uname` = "SunOS" ] ; then
+   echo "This test currently does not work on all flavors of Solaris."
+   exit 77
+fi
+if [ `uname` = "FreeBSD" ] ; then
+   echo "This test currently does not work on FreeBSD."
+   exit 77
+fi
+
+. ${srcdir}/test-framework.sh
+
+startup_receiver_valgrind
+startup_receiver_valgrind
+
+echo 'Send Message...'
+./send -t 127.0.0.1 -p $TESTPORT -m "testmessage"
+
+stop_receiver
+check_output "testmessage"
+terminate
