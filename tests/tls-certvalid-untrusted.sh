c0bb2f049f4386875056b4a587a5122800a2fb65
Rainer Gerhards <rgerhards@adiscon.com>
2026-08-19 18:00:22 +0200

    tests: isolate certvalid untrusted error log

--- tests/tls-certvalid-untrusted.sh
+++ tests/tls-certvalid-untrusted.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 . ${srcdir:=$(pwd)}/test-framework.sh
 function actual_test() {
+	local errorlog="${TESTDIR}/error.out.log"
 	startup_receiver -l $TEST_TLS_LIB -T -a "certvalid" \
 		-x ${srcdir}/tls-certs/ossl-ca.pem \
 		-y ${srcdir}/tls-certs/ossl-server-cert.pem \
 		-z ${srcdir}/tls-certs/ossl-server-key.pem \
-		-e error.out.log
+		-e "$errorlog"
 
 	echo 'Send Message with expired client certificate...'
-	./send -l $TEST_TLS_LIB -t 127.0.0.1 -p $TESTPORT -m "testmessage" -T -a "certvalid" \
+	if ./send -l $TEST_TLS_LIB -t 127.0.0.1 -p $TESTPORT -m "testmessage" -T -a "certvalid" \
 		-x ${srcdir}/tls-certs/ossl-ca.pem \
 		-y ${srcdir}/tls-certs/ossl-clientbrok-cert.pem \
 		-z ${srcdir}/tls-certs/ossl-clientbrok-key.pem \
-		$OPT_VERBOSE 1>>${OUTFILE} 2>&1
-	if [ $? -eq 0 ]; then
+		$OPT_VERBOSE 1>>${OUTFILE} 2>&1; then
 		printf 'FAIL: send succeeded with an invalid certificate in certvalid mode\n'
 		stop_receiver
 		exit 1
 	fi
 
 	stop_receiver
-	check_output "certificate validation failed" error.out.log
+	check_output "certificate validation failed" "$errorlog"
 }
 
 do_tls_subtests
